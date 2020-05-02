cc701aeb3c708c49ac20707e83cc33bb47e8b11e
tatwickutkarsh <tatwick.utkarsh@gmail.com>
2020-05-02 18:11:29 +0530

    Initial Commit

---
+++ student/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from student import views
+
+
+router = DefaultRouter()
+router.register('profile',views.UserProfileViewSet)
+router.register('complaint',views.UserComplaintViewSet)
+
+urlpatterns = [
+    path('login/',views.UserLoginApiView.as_view()),
+    path('',include(router.urls)),
+]
